f9c85c6ba909c1fa7fd003e4d588fcc3c2094722
majunliang <875657453@qq.com>
2023-07-15 05:47:01 +0800

    Initial commit

---
+++ EasyPay/src/main/java/com/example/easypay/EasyPayApplication.java
@@ -0,0 +1,13 @@
+package com.example.easypay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyPayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasyPayApplication.class, args);
+    }
+
+}
---
+++ EasyPay/src/test/java/com/example/easypay/EasyPayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.easypay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyPayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
